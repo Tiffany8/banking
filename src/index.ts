61c7cda9bc3e8821072251cc4c7d8ec66f05cafe
Tiffany R. Williams <trowilliams@gmail.com>
2023-10-09 01:39:52 -0400

    build(docker+server): Dockerize app and allow external connections
    - Created Dockerfile and docker-compose.yml for containerization.
    - Updated server.listen to accept connections from all network interfaces.

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ const start: () => Promise<void> = async (): Promise<void> => {
 
   try {
     await server.ready()
-    await server.listen({ port: 8080 })
+    await server.listen({ port: 8080, host: '0.0.0.0' })
   } catch (err) {
     server.log.error(err)
     process.exit(1)
